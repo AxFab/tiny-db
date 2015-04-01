4fd835edae7ce3718d9e0a5899a9dd05cc4b3275
Fabien Bavent <fabien.bavent@gmail.com>
2015-04-01 23:18:43 +0200

    [+] Add sharding object

--- index.js
+++ index.js
@@ -1,81 +1,32 @@
 
 var assert = require('assert'),
-    ClientDb = require('./lib/client.js')
+    DatasetDb = require('./lib/dataset.js'),
+    StorageDb = require('./lib/storage.js')
 
-// This is the example presented on MongoDb node.js client README page
-// The API of tiny-db is compatible with MongoDB (for now at least)
 
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.insert([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the document collection");
-    callback(result);
-  });
+var db = { storage: new StorageDb('./db/', { compress:false} ) }
+
+var idAuto = 1564
+db.newuid = function () {
+  return '_Z' + (idAuto++)
 }
 
-var updateDocument = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Update document where a is 2, set b equal to 1
-  collection.update({ a : 2 }
-    , { $set: { b : 1 } }, function(err, result) {
-    assert.equal(err, null);
-    assert.equal(1, result.result.n);
-    console.log("Updated the document with the field a equal to 2");
-    callback(result);
-  });  
-}
-
-var removeDocument = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.remove({ a : 3 }, function(err, result) {
-    assert.equal(err, null);
-    assert.equal(1, result.result.n);
-    console.log("Removed the document with the field a equal to 3");
-    callback(result);
-  });    
-}
+var data = DatasetDb(db, {
+  name: 'users',
+  capacity: 50,
+})
 
 
-var findDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    assert.equal(2, docs.length);
-    console.log("Found the following records");
-    console.dir(docs);
-    callback(docs);
-  });
-}
+data.insert ( { login:'fabien', value:843 }, function(err, res) {
+  console.log ('M', res)
+})
 
+data.insert ( [
+  { login:'anne', value:732 },
+  { login:'emma', value:379 },
+  ], function(err, res) {
+  console.log ('F', res)
+})
 
-// Connection URL
-var url = 'no persistant database, yet';
-// Use connect method to connect to the Server
-ClientDb.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-
-  insertDocuments(db, function() {
-    updateDocument(db, function() {
-      removeDocument(db, function() {
-        findDocuments(db, function() {
-          db.close();
-        });
-      });
-    });
-  });
-});
 
 
